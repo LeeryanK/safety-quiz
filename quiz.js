0f3e63b01e6cb4477d43c8773ec2559b052cae8c
Kyle Lin <kylejlin.us@gmail.com>
2016-10-01 11:12:05 -0700

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,2 +1,5 @@
-loader.render('test-quiz.json', document.getElementById('personality'));
+loader.render('test-quiz.json', document.getElementById('personality'), function(response) {
+  console.log('Here is our response...');
+  console.log(response);
+});
 console.log('Rendering...');
